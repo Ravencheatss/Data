97b28963af4bbc5e29536db2b955fac8afd1f355
Ravencheatss <ravencheatss@gmail.com>
2024-08-11 19:00:13 +0300

    baza.lua

--- baza.lua
+++
@@ -1,5 +0,0 @@
-return {
-    {login = "user1", password = "pass1", expiration = os.time({year=2025, month=12, day=31})},
-    {login = "user2", password = "pass2", expiration = os.time({year=2024, month=8, day=31})}
-}
-

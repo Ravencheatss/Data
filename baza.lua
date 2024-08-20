fea53f59309da062ca7428f685a226ce30c58175
Ravencheatss <ravencheatss@gmail.com>
2024-08-20 20:36:42 +0300

    Add files via upload

---
+++ baza.lua
@@ -0,0 +1,5 @@
+users = {
+    ["raven"] = {password = "iop", expiry_date = "2024-08-18"},
+    ["user2"] = {password = "pass2", expiry_date = "2024-09-01"},
+    -- добавьте других пользователей здесь
+}

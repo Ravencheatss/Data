d03dde1afdf5014e574cd0f1ee3d947fe09aca13
Ravencheatss <ravencheatss@gmail.com>
2024-08-20 21:16:57 +0300

    baza.lua

--- baza.lua
+++ baza.lua
@@ -1,12 +1,3 @@
-users = {
-  {
-    login = "user1",
-    password = "pass1",
-    expiry_date = "2024-08-30"
-  },
-  {
-    login = "user2",
-    password = "pass2",
-    expiry_date = "2025-12-31"
-  }
-}
+login = "mylogin"
+password = "mypassword"
+expiry_date = "2024-08-30"

4b8e114e5ccfc9a41bf3c2d50ad225f472008ac1
Ravencheatss <ravencheatss@gmail.com>
2024-08-20 22:27:12 +0300

    baza.lua

--- baza.lua
+++ baza.lua
@@ -1,7 +1,7 @@
 return {
     script_expiry_date = "2023-12-31", 
     users = {
-        { login = "user", password = "password", expiry_date = "2024-01-01" },
+        { login = "user", password = "password", expiry_date = "2025-01-01" },
         { login = "userr", password = "passwordd", expiry_date = "2024-08-31" },
     }
 }

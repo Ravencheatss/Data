32c19c6326fe4d31e3c7f00866a226cb34f1d67f
Ravencheatss <ravencheatss@gmail.com>
2024-08-20 20:45:17 +0300

    baza.lua

--- baza.lua
+++ baza.lua
@@ -1,5 +1,4 @@
 users = {
     ["raven"] = {password = "iop", expiry_date = "2024-08-18"},
     ["user2"] = {password = "pass2", expiry_date = "2024-09-01"},
-    -- добавьте других пользователей здесь
 }

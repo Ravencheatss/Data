78d2659169b484aed0dae9097a289d0b5c26a307
Ravencheatss <ravencheatss@gmail.com>
2024-08-28 13:20:12 +0300

    baza.lua

--- baza.lua
+++ baza.lua
@@ -1,70 +1,70 @@
 return {
-    script_expiry_date = "2024-12-31 23:59", 
+    script_expiry_date = "2024-12-31", 
     users = {
         { 
             login = "raven", 
             password = "madman", 
-            expiry_date = "2024-12-31 23:59" 
+            expiry_date = "2024-12-31" 
         },
         { 
             login = "artik", 
             password = "star", 
-            expiry_date = "2024-12-31 18:00" 
+            expiry_date = "2024-12-31" 
         },
         { 
             login = "drakosha", 
             password = "drakon", 
-            expiry_date = "2024-08-27 18:00" 
+            expiry_date = "2024-08-27" 
         },
         { 
             login = "ZEA", 
             password = "Raven", 
-            expiry_date = "2024-08-27 18:00" 
+            expiry_date = "2024-08-27" 
         },
         { 
             login = "Egorka", 
             password = "Anapa2008", 
-            expiry_date = "2024-08-27 18:00" 
+            expiry_date = "2024-08-27" 
         },
         { 
             login = "amir777", -- купил
             password = "norton91", 
-            expiry_date = "2024-09-29 18:00" 
+            expiry_date = "2024-09-29" 
         },    
         { 
             login = "Samir3749", 
             password = "Derty3434", 
-            expiry_date = "2024-08-27 18:00" 
+            expiry_date = "2024-08-27" 
         },    
         { 
             login = "Derty3434", 
             password = "cacrodi1", 
-            expiry_date = "2024-08-27 18:00" 
+            expiry_date = "2024-08-27" 
         }, 
         { 
             login = "Raychik", 
             password = "Raychik2288", 
-            expiry_date = "2024-08-27 18:00" 
+            expiry_date = "2024-08-27" 
         },  
         { 
             login = "Angelinka_Legion", 
             password = "1qaz0plm", 
-            expiry_date = "2024-08-27 18:00" 
+            expiry_date = "2024-08-27" 
         },      
         { 
             login = "pomidor", 
             password = "krutoy", 
-            expiry_date = "2024-09-04 18:00" 
+            expiry_date = "2024-09-04" 
         }, 
         { 
             login = "Tesak2010", 
             password = "Mishanya2010", 
-            expiry_date = "2024-08-27 18:00" 
+            expiry_date = "2024-08-27" 
         },     
         { 
             login = "Sigma", -- купил
             password = "Sotka228", 
-            expiry_date = "2024-08-30 18:00" 
+            expiry_date = "2024-08-30" 
         },         
     }
 }

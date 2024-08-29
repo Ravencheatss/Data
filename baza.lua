359b72a5ef7456651418cd1b07866aa409dae7ea
Ravencheatss <ravencheatss@gmail.com>
2024-08-29 12:13:34 +0300

    baza.lua

--- baza.lua
+++ baza.lua
@@ -12,54 +12,14 @@ return {
             expiry_date = "2024-12-31" 
         },
         { 
-            login = "drakosha", 
-            password = "drakon", 
-            expiry_date = "2024-08-27" 
-        },
-        { 
-            login = "ZEA", 
-            password = "Raven", 
-            expiry_date = "2024-08-27" 
-        },
-        { 
-            login = "Egorka", 
-            password = "Anapa2008", 
-            expiry_date = "2024-08-27" 
-        },
-        { 
             login = "amir777", -- купил
             password = "norton91", 
             expiry_date = "2024-09-29" 
-        },    
-        { 
-            login = "Samir3749", 
-            password = "Derty3434", 
-            expiry_date = "2024-08-27" 
-        },    
-        { 
-            login = "Derty3434", 
-            password = "cacrodi1", 
-            expiry_date = "2024-08-27" 
-        }, 
-        { 
-            login = "Raychik", 
-            password = "Raychik2288", 
-            expiry_date = "2024-08-27" 
-        },  
-        { 
-            login = "Angelinka_Legion", 
-            password = "1qaz0plm", 
-            expiry_date = "2024-08-27" 
-        },      
+        },          
         { 
             login = "pomidor", 
             password = "krutoy", 
             expiry_date = "2024-09-04" 
-        }, 
-        { 
-            login = "Tesak2010", 
-            password = "Mishanya2010", 
-            expiry_date = "2024-08-27" 
         },     
         { 
             login = "Sigma", -- купил

3d1cbac5ad89ce4aa25fc627c59788b1fc24dba4
Ravencheatss <ravencheatss@gmail.com>
2024-09-14 20:18:43 +0300

    Add files via upload

---
+++ baza1.lua
@@ -0,0 +1,45 @@
+return {
+    script_expiry_date = "2024-12-31", 
+    users = {
+        { 
+            login = "raven", 
+            password = "madman", 
+            expiry_date = "2024-12-31" 
+        },
+        { 
+            login = "artik", 
+            password = "star", 
+            expiry_date = "2024-12-31" 
+        },
+        { 
+            login = "amir777", -- купил
+            password = "norton91", 
+            expiry_date = "2024-09-29" 
+        },          
+        { 
+            login = "FREE", 
+            password = "FREE3", 
+            expiry_date = "2024-09-13" 
+        },     
+        { 
+            login = "Inercia", -- купил
+            password = "Inercia328", 
+            expiry_date = "2024-09-13" 
+        },
+        { 
+            login = "idevil2018", -- купил
+            password = "13082004", 
+            expiry_date = "2024-09-29" 
+        },
+        { 
+            login = "32bit", -- подарил
+            password = "32bit1023", 
+            expiry_date = "2024-09-29" 
+        },   
+        { 
+            login = "Egorka", -- подарил
+            password = "Anapa2008", 
+            expiry_date = "2024-09-29" 
+        },           
+    }
+}

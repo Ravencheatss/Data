c2719c47dd5e327e0d1ab9c3d697133490023190
Ravencheatss <ravencheatss@gmail.com>
2024-10-09 15:39:36 +0300

    newbaza.lua

--- newbaza.lua
+++ newbaza.lua
@@ -5,55 +5,55 @@ return {
             login = "raven", 
             password = "madman", 
             is_active = true, 
-            note = "No expiration date" 
+            note = "2024-12-31" 
         },
         { 
             login = "artik", 
             password = "star", 
             is_active = true, 
-            note = "No expiration date" 
+            note = "2024-12-31" 
         },
         { 
             login = "amir777", -- купил
             password = "norton91", 
             is_active = false, 
-            note = "Expired on 2024-10-02" 
+            note = "2024-10-02" 
         },          
         { 
             login = "free", 
             password = "free", 
             is_active = true, 
-            note = "No expiration date" 
+            note = "2024-10-02" 
         },     
         { 
             login = "santik", -- купил
             password = "santik667", 
             is_active = false, 
-            note = "Expired on 2024-09-30" 
+            note = "2024-09-30" 
         },
         { 
             login = "tresh", -- купил
             password = "tresh5491", 
             is_active = false, 
-            note = "Expired on 2024-10-27" 
+            note = "2024-10-27" 
         },
         { 
             login = "idevil2018", -- купил
             password = "13082004", 
             is_active = false, 
-            note = "Expired on 2024-10-02" 
+            note = "2024-10-02" 
         },
         { 
             login = "32bit", -- подарил
             password = "32bit1023", 
-            is_active = true, 
-            note = "No expiration date" 
+            is_active = false, 
+            note = "2024-09-29" 
         },   
         { 
             login = "Egorka", -- подарил
             password = "Anapa2008", 
-            is_active = true, 
-            note = "No expiration date" 
+            is_active = false, 
+            note = "2024-09-29" 
         },           
     }
 }

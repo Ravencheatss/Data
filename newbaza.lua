3b8189059130ab0ea1b30b50b57b546b28481b3e
Ravencheatss <ravencheatss@gmail.com>
2024-10-09 15:23:59 +0300

    Add files via upload

---
+++ newbaza.lua
@@ -0,0 +1,59 @@
+return {
+    script_enabled = true, 
+    users = {
+        { 
+            login = "raven", 
+            password = "madman", 
+            is_active = true, 
+            note = "No expiration date" 
+        },
+        { 
+            login = "artik", 
+            password = "star", 
+            is_active = true, 
+            note = "No expiration date" 
+        },
+        { 
+            login = "amir777", -- купил
+            password = "norton91", 
+            is_active = false, 
+            note = "Expired on 2024-10-02" 
+        },          
+        { 
+            login = "free", 
+            password = "free", 
+            is_active = true, 
+            note = "No expiration date" 
+        },     
+        { 
+            login = "santik", -- купил
+            password = "santik667", 
+            is_active = false, 
+            note = "Expired on 2024-09-30" 
+        },
+        { 
+            login = "tresh", -- купил
+            password = "tresh5491", 
+            is_active = false, 
+            note = "Expired on 2024-10-27" 
+        },
+        { 
+            login = "idevil2018", -- купил
+            password = "13082004", 
+            is_active = false, 
+            note = "Expired on 2024-10-02" 
+        },
+        { 
+            login = "32bit", -- подарил
+            password = "32bit1023", 
+            is_active = true, 
+            note = "No expiration date" 
+        },   
+        { 
+            login = "Egorka", -- подарил
+            password = "Anapa2008", 
+            is_active = true, 
+            note = "No expiration date" 
+        },           
+    }
+}

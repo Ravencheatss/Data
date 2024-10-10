eb3233de267c600dacd1c3d7eb50732fbf67c1b7
Ravencheatss <ravencheatss@gmail.com>
2024-10-10 18:14:00 +0300

    newfree.lua

--- newfree.lua
+++ newfree.lua
@@ -1,1 +1,12 @@
-
+return {
+    script_enabled = false, 
+    users = {  
+        { 
+            login = "free", -- подарил
+            password = "free", 
+            is_active = false, 
+            note = "2024-09-29", 
+            device_id = ""
+        },           
+    }
+}

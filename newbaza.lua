8d18f84dbac0265a392c5cfd606f0a2261bcf12a
Ravencheatss <ravencheatss@gmail.com>
2024-10-10 14:24:17 +0300

    newbaza.lua

--- newbaza.lua
+++ newbaza.lua
@@ -5,55 +5,64 @@ return {
             login = "raven", 
             password = "madman", 
             is_active = true, 
-            note = "2024-12-31" 
+            note = "2024-12-31",
+            device_id = ""
         },
         { 
             login = "artik", 
             password = "star", 
             is_active = true, 
-            note = "2024-12-31" 
+            note = "2024-12-31",
+            device_id = ""
         },
         { 
             login = "amir777", -- купил
             password = "norton91", 
             is_active = false, 
-            note = "2024-10-02" 
+            note = "2024-10-02",
+            device_id = ""
         },          
         { 
             login = "free", 
             password = "free", 
             is_active = true, 
-            note = "2024-10-02" 
+            note = "2024-10-02", 
+            device_id = ""
         },     
         { 
             login = "santik", -- купил
             password = "santik667", 
             is_active = false, 
-            note = "2024-09-30" 
+            note = "2024-09-30", 
+            device_id = ""
         },
         { 
             login = "tresh", -- купил
             password = "tresh5491", 
             is_active = false, 
-            note = "2024-10-27" 
+            note = "2024-10-27", 
+            device_id = ""
         },
         { 
             login = "idevil2018", -- купил
             password = "13082004", 
             is_active = false, 
-            note = "2024-10-02" 
+            note = "2024-10-02", 
+            device_id = ""
         },
         { 
             login = "32bit", -- подарил
             password = "32bit1023", 
             is_active = false, 
-            note = "2024-09-29" 
+            note = "2024-09-29", 
+            device_id = ""
         },   
         { 
             login = "Egorka", -- подарил
             password = "Anapa2008", 
             is_active = false, 
-            note = "2024-09-29" 
+            note = "2024-09-29", 
+            device_id = ""
         },           
     }
 }
